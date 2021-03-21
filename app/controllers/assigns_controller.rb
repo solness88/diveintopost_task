f3183a3383a04f5d77ad9689d3c239ce499f22f6
hirokazu kobayashi <solness.stf@gmail.com>
2021-03-21 23:28:39 +0900

    Teamの権限移動機能を実装

--- app/controllers/assigns_controller.rb
+++ app/controllers/assigns_controller.rb
@@ -42,7 +42,9 @@ class AssignsController < ApplicationController
 
   def email_exist?
     team = find_team(params[:team_id])
-    redirect_to team_url(team), notice: I18n.t('views.messages.email_already_exists') if team.members.exists?(email: params[:email])
+    if team.members.exists?(email: params[:email])
+      redirect_to team_url(team), notice: I18n.t('views.messages.email_already_exists')
+    end
   end
 
   def email_reliable?(address)
@@ -59,7 +61,7 @@ class AssignsController < ApplicationController
     change_keep_team(assigned_user, another_team) if assigned_user.keep_team_id == assign.team_id
   end
 
-  # def find_team(_team_id)
-  # team = Team.friendly.find(params[:team_id])
-  # end
+  def find_team(_team_id)
+    team = Team.friendly.find(params[:team_id])
+  end
 end
--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,6 +1,6 @@
 class TeamsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_team, only: %i[show edit update destroy]
+  before_action :set_team, only: %i[show edit update destroy change_owner]
 
   def index
     @teams = Team.all
@@ -45,6 +45,15 @@ class TeamsController < ApplicationController
 
   def dashboard
     @team = current_user.keep_team_id ? Team.find(current_user.keep_team_id) : current_user.teams.first
+  end
+
+  def change_owner
+    @team[:owner_id] = Assign.find(params[:format]).user_id
+    if @team.update(team_params)
+      redirect_to @team, notice: I18n.t('views.messages.change_ownerid')
+    else
+      redirect_to @team, notice: I18n.t('views.messages.change_ownerid_failed')
+    end
   end
 
   private
