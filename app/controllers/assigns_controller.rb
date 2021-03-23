9415c28b6211219d92f264f06356364d60524978
hirokazu kobayashi <solness.stf@gmail.com>
2021-03-23 23:55:17 +0900

    rubocopチェック

--- app/controllers/assigns_controller.rb
+++ app/controllers/assigns_controller.rb
@@ -42,9 +42,7 @@ class AssignsController < ApplicationController
 
   def email_exist?
     team = find_team(params[:team_id])
-    if team.members.exists?(email: params[:email])
-      redirect_to team_url(team), notice: I18n.t('views.messages.email_already_exists')
-    end
+    redirect_to team_url(team), notice: I18n.t('views.messages.email_already_exists') if team.members.exists?(email: params[:email])
   end
 
   def email_reliable?(address)
@@ -61,7 +59,7 @@ class AssignsController < ApplicationController
     change_keep_team(assigned_user, another_team) if assigned_user.keep_team_id == assign.team_id
   end
 
-  def find_team(_team_id)
-    team = Team.friendly.find(params[:team_id])
-  end
+  # def find_team(_team_id)
+  # team = Team.friendly.find(params[:team_id])
+  # end
 end
